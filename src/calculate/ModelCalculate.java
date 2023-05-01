69d2161b842c2b4ed2ae7031516524072c3629f7
Zac-AND-Digital <zac.king@and.digital>
2023-05-01 16:03:46 +0100

    Modify ModelCalculate to pass tests

--- src/calculate/ModelCalculate.java
+++ src/calculate/ModelCalculate.java
@@ -3,19 +3,19 @@ package calculate;
 public class ModelCalculate {
 
     long add(long x, long y) {
-        return 0L; // TODO: Implement Method
+        return x + y;
     }
 
     long subtract(long x, long y) {
-        return 0L; // TODO: Implement Method
+        return x - y;
     }
 
     double multiply(double x, double y) {
-        return 0.0d; // TODO: Implement Method
+        return x * y;
     }
 
     double divide(double x, double y) {
-        return 0.0d; // TODO: Implement Method
+        return x / y;
     }
 
 }
